6f4083f58a6eecf60936173cac27aacbc77a2ed0
Rabi <rabif12@aol.com>
2022-01-07 05:07:12 -0500

    completed javascript

--- video-bg.js
+++ video-bg.js
@@ -3,3 +3,12 @@ window.addEventListener('click', () => {
     console.log("working " + counter);
     counter++;
 });
+
+
+const menuToggle = document.querySelector('.toggle');
+const showcase = document.querySelector('.showcase');
+
+menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active');
+    showcase.classList.toggle('active');
+});
