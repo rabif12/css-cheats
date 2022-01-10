cdcd105f5244ce20b09f144305f4ce100a4e3efc
Rabi <rabif12@aol.com>
2022-01-10 03:34:48 -0500

    basics added with js

--- 3d-card/3d-card.js
+++ 3d-card/3d-card.js
@@ -4,3 +4,14 @@ window.addEventListener('click', () => {
     console.log("clicked" + counter);
     counter++;
 })
+
+//movement animation
+const card = document.querySelector('.card');
+var container = document.querySelector('.container');
+
+container.addEventListener('mousemove', (e) => {
+    let xAxis = (window.innerWidth / 2 - e.pageX)/10;
+    let yAxis = (window.innerHeight / 2 - e.pageY)/10;
+
+    card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
