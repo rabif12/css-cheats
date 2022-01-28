12656c6a9a4ec7877743e29fb7b299d0eed3d2d7
Rabi <rabif12@aol.com>
2022-01-28 04:03:08 -0500

    fixed nav

--- portfolio-1/portfolio-1.js
+++ portfolio-1/portfolio-1.js
@@ -6,12 +6,15 @@ window.addEventListener('click', () => {
 
 /*===vanilla js sticky nav using scroll page Y===*/
 var navbar = document.querySelector(".navbar");
+var bgcolor = document.querySelector(".max-width");
 
 function fixnav() {
     if (window.scrollY >= 100) {
         navbar.classList.add("added");
+        bgcolor.classList.remove("bgcolor");
     } else {
         navbar.classList.remove("added");
+        bgcolor.classList.add("bgcolor");
     };
 };
 
