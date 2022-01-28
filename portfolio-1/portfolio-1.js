667f163e360054d1fb6fdde6e8329d656a7125fe
Rabi <rabif12@aol.com>
2022-01-28 03:32:30 -0500

    functional nav vanilla js added

--- portfolio-1/portfolio-1.js
+++ portfolio-1/portfolio-1.js
@@ -3,3 +3,16 @@ window.addEventListener('click', () => {
     console.log("clicked " + counter);
     counter++;
 });
+
+/*===vanilla js sticky nav using scroll page Y===*/
+var navbar = document.querySelector(".navbar");
+
+function fixnav() {
+    if (window.scrollY >= 100) {
+        navbar.classList.add("added");
+    } else {
+        navbar.classList.remove("added");
+    };
+};
+
+window.addEventListener('scroll', fixnav);
