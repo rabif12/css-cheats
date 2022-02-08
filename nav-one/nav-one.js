e783ebc70d120800d5a6c317f84054e7192bc55a
Rabi <rabif12@aol.com>
2022-02-08 07:07:00 -0500

    added js

--- nav-one/nav-one.js
+++ nav-one/nav-one.js
@@ -3,3 +3,21 @@ window.addEventListener("click", () => {
     console.log("clicked " + counter);
     counter++;
 });
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.navlinks li');
+    //toggle nav
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+    });
+    //animate links
+    navLinks.forEach((link, index) => {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.2}s`;
+    });
+};
+
+
+
+navSlide();
